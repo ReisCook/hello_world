f01810915f52de00e54adb90a74fe6ccd31e2c34
mendoken <138827708+mendoken@users.noreply.github.com>
2024-01-30 08:46:57 -0800

    Update Main.java
    Added kenneth's hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Kenneth's Hello world! 1/30/23 08:46");
     }
 }
